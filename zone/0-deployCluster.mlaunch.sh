6a3daf9eb2de7c3970cc1b0c5669616fde845389
Jonathan Janos <janosj@yahoo.com>
2023-09-01 14:42:10 -0400

    added mlaunch

---
+++ zone/0-deployCluster.mlaunch.sh
@@ -0,0 +1,16 @@
+# Deploy cluster using mlaunch.
+# https://rueckstiess.github.io/mtools/install.html
+# https://rueckstiess.github.io/mtools/mlaunch.html
+
+# Deploys a Sharded Cluster with 2 shards.
+# Single-node replica sets are specified for both shards as well as the CSRS.
+mlaunch --replicaset --nodes 1 --sharded 2 --config 1 --binarypath $HOME/.local/m/versions/6.0.6-ent/bin
+
+echo
+echo "Done. To list processes:"
+echo "  mlaunch list --startup"
+echo
+
+echo "MongoDB data files are stored in ./data."
+echo
+
---
+++ zone/killCluster.sh
@@ -0,0 +1,8 @@
+mlaunch kill --verbose
+
+echo
+echo "Done, but you may need to wait a bit."
+echo "Check using 'ps -ef | grep mongod'"
+echo "Afterwards, you may delete the ./data directory."
+echo
+
